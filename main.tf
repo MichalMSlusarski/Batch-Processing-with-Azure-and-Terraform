860c4d4b7d004c10f284061193944922c3d227e2
Michał Ślusarski <115535646+MichalMSlusarski@users.noreply.github.com>
2023-11-29 23:48:51 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,3 +5,51 @@ provider "azurerm" {
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
 }
+
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "your_resource_group_name"
+    storage_account_name  = "your_storage_account_name"
+    container_name        = "your_container_name"
+    key                   = "terraform.tfstate"
+  }
+}
+
+# Azure Storage Account and Blob Container
+resource "azurerm_storage_account" "example_storage" {
+  name                     = "examplestorageaccount"
+  resource_group_name      = azurerm_resource_group.example_rg.name
+  location                 = "East US"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "gameplay_container" {
+  name                  = "gameplaycontainer"
+  storage_account_name  = azurerm_storage_account.example_storage.name
+  container_access_type = "private"
+}
+
+# Azure Functions
+resource "azurerm_function_app" "example_function" {
+  # Configuration for Azure Functions
+  # ...
+}
+
+# Azure Data Factory
+resource "azurerm_data_factory" "example_data_factory" {
+  # Configuration for Azure Data Factory
+  # ...
+}
+
+# Azure SQL Database
+resource "azurerm_sql_server" "example_sql_server" {
+  # Configuration for SQL Server
+  # ...
+}
+
+resource "azurerm_sql_database" "example_sql_db" {
+  # Configuration for SQL Database
+  # ...
+}
+
