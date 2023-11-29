814d9c89e646120eaee221301b7193ad418cca67
Michał Ślusarski <115535646+MichalMSlusarski@users.noreply.github.com>
2023-11-29 22:54:49 +0100

    Create tables.sql

---
+++ tables.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Users (
+    user_id INT PRIMARY KEY,
+    age INT,
+    gender BOOL,
+    -- Add other columns for city, state, country, preferred_genres, etc.
+    -- Example: city VARCHAR(50), state VARCHAR(50), country VARCHAR(50), preferred_genres TEXT,
+    -- ...
+);
