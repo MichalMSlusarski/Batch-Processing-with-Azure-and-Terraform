a5d3cd394b8c6919a500c4272d5d5153c67f3c72
Michał Ślusarski <115535646+MichalMSlusarski@users.noreply.github.com>
2023-12-03 18:21:01 +0100

    Update upload.py

--- upload.py
+++ upload.py
@@ -1,7 +1,32 @@
 import os
-from azure.storage.blob import BlobServiceClient, BlobClient, ContentSettings
+from azure.storage.blob import BlobServiceClient
 
-def upload_files_to_blob_storage(session_id, temp_folder_path):
+def read_settings_file(settings_file):
+    settings = {}
+    with open(settings_file, 'r') as file:
+        for line in file:
+            key, value = line.strip().split('=')
+            settings[key] = value
+    return settings
+
+def upload_files_to_blob_storage(settings_file):
+    # Read settings from settings file
+    settings = read_settings_file(settings_file)
+    active_session_path = settings.get('active_session_path')
+
+    if not active_session_path:
+        print("Error: 'active_session_path' not found in settings.")
+        return
+
+    active_session_folders = os.listdir(active_session_path)
+
+    if len(active_session_folders) != 1: 
+        print("Error: There should be exactly one folder inside 'active_session'.")
+        return
+
+    session_id = active_session_folders[0]
+    session_folder_path = os.path.join(active_session_path, session_id)
+
     # Azure Blob Storage credentials
     connection_string = "YOUR_AZURE_STORAGE_CONNECTION_STRING"
 
@@ -9,7 +34,7 @@ def upload_files_to_blob_storage(session_id, temp_folder_path):
     blob_service_client = BlobServiceClient.from_connection_string(connection_string)
 
     # Create a container named after the session ID if it doesn't exist
-    container_name = session_id.lower()  # Ensure lowercase for container name
+    container_name = session_id.lower()
     container_client = blob_service_client.get_container_client(container_name)
     if not container_client.exists():
         container_client.create_container()
@@ -22,7 +47,7 @@ def upload_files_to_blob_storage(session_id, temp_folder_path):
 
     missing_files = []
     for file_name in files_to_upload:
-        file_path = os.path.join(temp_folder_path, file_name)
+        file_path = os.path.join(session_folder_path, file_name)
         if os.path.exists(file_path):
             # Upload file to Azure Blob Storage
             blob_client = blob_service_client.get_blob_client(container=container_name, blob=file_name)
@@ -36,9 +61,12 @@ def upload_files_to_blob_storage(session_id, temp_folder_path):
         print(", ".join(missing_files))
         # You can also notify the user or handle the missing files according to your requirements
 
-# Usage
-if __name__ == "__main__":
-    session_id = "YOUR_SESSION_ID"  # Replace with your session ID
-    temp_folder_path = "PATH_TO_TEMP_FOLDER"  # Replace with your temporary folder path
+    # Delete the session folder after successful upload
+    if os.path.exists(session_folder_path):
+        os.rmdir(session_folder_path)
+        print(f"Session folder '{session_id}' deleted after successful upload.")
 
-    upload_files_to_blob_storage(session_id, temp_folder_path)
+if __name__ == "__main__":
+    settings_file_path = "settings.txt"
+
+    upload_files_to_blob_storage(settings_file_path)
